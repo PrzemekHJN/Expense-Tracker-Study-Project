9a43d82f18284dbc562c5e7679f7776e0854af75
PrzemekHJN <przemyslaw.hojny@gmail.com>
2024-06-02 14:15:23 +0200

    updated version, fixed database connection

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+
+
+    public class ApplicationDbContext: DbContext
+    {
+        public IConfiguration _config {  get; set; }
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        // public ApplicationDbContext(DbContextOptions options):base(options)
+        // {           
+        // }
+
+    }
+}
