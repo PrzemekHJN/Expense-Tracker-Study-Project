f4f6daee55e7dcd870e52019e84d50798a7ee3fa
PrzemekHJN <przemyslaw.hojny@gmail.com>
2024-06-05 20:40:06 +0200

    grid, fonts, sorting, action column

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -24,23 +24,6 @@ namespace ExpenseTracker.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id=0)
@@ -71,23 +54,6 @@ namespace ExpenseTracker.Controllers
         }
 
         
-                // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -104,9 +70,5 @@ namespace ExpenseTracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-            return _context.Categories.Any(e => e.CategoryId == id);
-        }
     }
 }
