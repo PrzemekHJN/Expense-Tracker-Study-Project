c7c6cd88e6828a3967abddd71bab29682f75cdf2
PrzemekHJN <przemyslaw.hojny@gmail.com>
2024-06-12 16:54:31 +0200

    spline chart properties

--- ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80cab09b6196bfba177075ae8a4de40bae75f691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d970f97ddff62709f026f8327fbd3b90b6177b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
